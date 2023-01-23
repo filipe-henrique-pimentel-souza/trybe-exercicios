6c92d8c50c8880bc4b1e512684f857974c9b6a28
Filipe Henrique Pimentel Souza <advogado.filipehps@gmail.com>
2023-01-23 13:20:13 -0300

    Exercícios pré-aula

---
+++ fundamentos/secao-03-introducao-javascript/script2.js
@@ -0,0 +1,19 @@
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+const base = 5;
+const height = 8;
+let area = base * height;
+console.log(area);
+let perimeter = (base * 2) + (height * 2);
+console.log(perimeter);
---
+++ fundamentos/secao-03-introducao-javascript/script3.js
@@ -0,0 +1,9 @@
+const nota = 100;
+
+if (nota >= 80){
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+} else if (nota >= 60 && nota < 80){
+    console.log("Você está na nossa lista de espera");
+} else {
+    console.log("Infelizmente, você reprovou.");
+}
