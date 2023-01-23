b506b9e25a3ed40a7bc063366e27644329fa0b99
Filipe Henrique Pimentel Souza <advogado.filipehps@gmail.com>
2023-01-23 10:21:08 -0300

    Início da seção de Javascript

---
+++ fundamentos/secao-03-introducao-javascript/script.js
@@ -0,0 +1,8 @@
+const myName = 'Filipe Henrique Pimentel Souza';
+const birthCity = 'Vitória';
+let birthYear = 1991;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
