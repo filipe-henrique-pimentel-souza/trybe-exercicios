0268533422f62a750b9904f85d54a5640b4d8dad
Filipe Henrique Pimentel Souza <advogado.filipehps@gmail.com>
2023-01-27 08:16:20 -0300

    Bônus 3, dia 04

---
+++ fundamentos/secao-03-introducao-javascript/objetos-e-funcoes-dia-04/bonus2.js
@@ -0,0 +1,40 @@
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+const cestaDeCompras = {
+};
+
+for (const i of basket) {
+    if (cestaDeCompras.hasOwnProperty(i)) {
+        cestaDeCompras[i] +=1
+    } else {
+        cestaDeCompras[i] = 1;
+    }
+}
+
+console.log(cestaDeCompras)
+
+let mensagem = 'Sua cesta possui:';
+
+for (const i in cestaDeCompras) {
+    mensagem += ' ' + cestaDeCompras[i] + ' ' + i + 's' + ','
+}
+mensagem = mensagem.replace(/,$/,'.');
+console.log(mensagem)
