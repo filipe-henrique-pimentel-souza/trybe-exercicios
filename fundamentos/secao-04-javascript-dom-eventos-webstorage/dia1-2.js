c41d70ccc7f71960ce753f8f348d7e9a53f68def
Filipe Henrique Pimentel Souza <advogado.filipehps@gmail.com>
2023-01-31 13:25:10 -0300

    Criação arquivos iniciais da semana

---
+++ fundamentos/secao-04-javascript-dom-eventos-webstorage/dia1-2.js
@@ -0,0 +1,5 @@
+const headContainer = document.querySelector('#header-container');
+headContainer.style.backgroundColor = 'rgb(0, 176, 105)';
+
+const emergencyTasks = document.querySelectorAll('.emergency-tasks')[0];
+emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
